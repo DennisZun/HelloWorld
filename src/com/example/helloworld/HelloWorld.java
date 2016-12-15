37d990225fe7d33909d76cfda422f521b9cc230b
D Zundert <dhpzundert@hotmail.com>
2016-12-15 22:35:12 +0100

    test commit 02

---
+++ src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+/**
+ * Created by Nolus on 15-12-2016.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! Dennis!");
+        System.out.println("it's me, wombat!");
+    }
+}
